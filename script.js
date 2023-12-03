a84087556fc9de3066d82aa6f1ea75395d2453a8
Wendel09Manfrs <manfriniwendel@gmail.com>
2023-12-03 20:03:28 -0300

    criacao de mais um tipo de particula

--- script.js
+++ script.js
@@ -17,11 +17,12 @@ const quantParticle = document.getElementById('qtdParticle')
 const botaoText = document.getElementById('mudaTextura')
 const botaoQtd = document.getElementById('mudaQtd')
 const corEsc = document.getElementById('cor')
+const corGaiola = document.getElementById('corGaiola')
 
-const blend = new URL('./blender/palavraPequeno.glb',import.meta.url)
+// const blend = new URL('./blender/palavraGrande.glb',import.meta.url)
 
 let selectedTexture = 'luz'
-let quantPart = 10000
+let quantPart = 5000
 let clique = false
 
 const textureMappings = {
@@ -50,7 +51,7 @@ camera = new THREE.PerspectiveCamera(
   2000000,
 )
 camera.position.z = 400
-camera.position.x = 200
+camera.position.x = 0
 camera.position.y = 0
 
 renderer = new THREE.WebGLRenderer()
@@ -61,16 +62,18 @@ const orbit = new OrbitControls(camera, renderer.domElement)
 orbit.enablePan = true
 orbit.enableRotate = true
 orbit.enableZoom = true
-const focusPoint = new THREE.Vector3(200, 0, 0) // Altere para o ponto que desejar
+const focusPoint = new THREE.Vector3(0, 50, 0) // Altere para o ponto que desejar
 orbit.target.copy(focusPoint)
 
-camera.lookAt(new THREE.Vector3(200, 0, 120))
+camera.lookAt(new THREE.Vector3(0, 50, 0))
 
 renderer.shadowMap.enabled = true
 
 let mixer;
 
 let positionWord = []
+let positionsFinal2 = [];
+
 
 function curva(s, angleQ, pi, num, deny, denx, spacey, spacex) {
   const rotationAngle = Math.PI / angleQ // Ângulo de rotação (90 graus)
@@ -117,12 +120,13 @@ function createLetterC(s) {
   curva(s, 0.36, 1.4, quantPart / 8, 1, 1.1, 40, 0) //1250
 }
 
-createLetterP(0)
-createLetterP(80)
-createLetterG(190)
-createLetterE(250)
-createLetterE(310)
-createLetterC(410)
+
+createLetterP(-225)
+createLetterP(-145)
+createLetterG(-35)
+createLetterE(25)
+createLetterE(85)
+createLetterC(185)
 
 function corAleatoria() {
   const r = Math.random()
@@ -130,16 +134,57 @@ function corAleatoria() {
   const b = Math.random()
   return new THREE.Color(r, g, b)
 }
+function gaiola() {
+  positions2 = []
+
+  for (let i = 0; i < 7000; i++) {
+    const phi = Math.acos(1 - 2 * Math.random()) // ângulo de inclinação
+    const theta = 2 * Math.PI * i / 80 // ângulo azimutal
+
+    const radius = 2000// raio da esfera oca
+    const x = radius * Math.sin(phi) * Math.cos(theta)
+    const y = 50+radius * Math.sin(phi) * Math.sin(theta)
+    const z = radius * Math.cos(phi)
+
+    positions2.push(new THREE.Vector3(x, y, z))
+  }
+  return { positions2}
+}
+
+
+function gaiolaFinal() {
+  positionsFinal2 = []
+
+  for (let i = 0; i < 5000; i++) {
+    const phi = Math.acos(1 - 2 * Math.random()) // ângulo de inclinação
+    const theta = 2 * Math.PI * i / 40 // ângulo azimutal
+
+    const radius = 130// raio da esfera oca
+    const x = 2*radius * Math.sin(phi) * Math.cos(theta)
+    const y = 50+radius * Math.sin(phi) * Math.sin(theta)
+    const z = radius/2 * Math.cos(phi)
+
+    positionsFinal2.push(new THREE.Vector3(x, y, z))
+  }
+}
+gaiolaFinal()
 
 let particleWordGeometry = new THREE.BufferGeometry().setFromPoints(
   positionWord,
 )
 
 let positionsAttribute = particleWordGeometry.getAttribute('position')
-
 let finalPositions = positionsAttribute.array
 
+let particleWordGeometry2 = new THREE.BufferGeometry().setFromPoints(
+  positionsFinal2,
+)
+
+let positionsAttribute2 = particleWordGeometry2.getAttribute('position')
+let finalPositions2 = positionsAttribute2.array
+
 let positions = []
+let positions2 = []
 function AleatParticle(qtd) {
   positions = []
   let colors = []
@@ -148,8 +193,8 @@ function AleatParticle(qtd) {
     const phi = Math.acos(2 * Math.random() - 1) // ângulo de inclinação
 
     const radius = Math.cbrt(Math.random()) * 2000
-    const x = 150 + radius * Math.sin(phi) * Math.cos(theta)
-    const y = 50 + radius * Math.sin(phi) * Math.sin(theta)
+    const x = radius * Math.sin(phi) * Math.cos(theta)
+    const y = 50+radius * Math.sin(phi) * Math.sin(theta)
     const z = radius * Math.cos(phi)
 
     positions.push(new THREE.Vector3(x, y, z))
@@ -158,6 +203,8 @@ function AleatParticle(qtd) {
   }
   return { positions, colors }
 }
+
+
 
 const particula = AleatParticle(quantPart)
 let particleGeometry = new THREE.BufferGeometry()
@@ -168,6 +215,17 @@ particleGeometry.setAttribute(
     3,
   ),
 )
+
+const particula2 = gaiola()
+let particleGeometry2 = new THREE.BufferGeometry()
+particleGeometry2.setAttribute(
+  'position',
+  new THREE.Float32BufferAttribute(
+    particula2.positions2.flatMap((v) => [v.x, v.y, v.z]),
+    3,
+  ),
+)
+
 particleGeometry.setAttribute(
   'color',
   new THREE.Float32BufferAttribute(
@@ -182,6 +240,12 @@ let positionsAttrAleat = particleGeometryAleat.getAttribute('position')
 
 let aleatPositions = positionsAttrAleat.array
 
+let particleGeometryAleat2 = new THREE.BufferGeometry().setFromPoints(positions2)
+
+let positionsAttrAleat2 = particleGeometryAleat2.getAttribute('position')
+
+let aleatPositions2 = positionsAttrAleat2.array
+
 const particleTexture = new THREE.TextureLoader().load(
   textureMappings[selectedTexture],
 )
@@ -195,8 +259,20 @@ const particleMaterial = new THREE.PointsMaterial({
   transparent:false
 })
 
+const particleMaterial2 = new THREE.PointsMaterial({
+  size: 8,
+  map:particleTexture,
+  blending: THREE.AdditiveBlending,
+  depthWrite: false,
+  transparent:false
+})
+
 let particles = new THREE.Points(particleGeometry, particleMaterial)
 scene.add(particles)
+
+
+let particles2 = new THREE.Points(particleGeometry2, particleMaterial2)
+scene.add(particles2)
 
 botaoQtd.addEventListener('click', function () {
   quantPart = quantParticle.value
@@ -225,12 +301,12 @@ botaoQtd.addEventListener('click', function () {
   aleatPositions = positionsAttrAleat.array
   particles = new THREE.Points(particleGeometry, particleMaterial) // Cria novos pontos
   scene.add(particles)
-  createLetterP(0)
-  createLetterP(80)
-  createLetterG(190)
-  createLetterE(250)
-  createLetterE(310)
-  createLetterC(410)
+  createLetterP(-225)
+  createLetterP(-145)
+  createLetterG(-35)
+  createLetterE(25)
+  createLetterE(85)
+  createLetterC(185)
 
   particleWordGeometry = new THREE.BufferGeometry().setFromPoints(positionWord)
 
@@ -238,14 +314,13 @@ botaoQtd.addEventListener('click', function () {
 
   finalPositions = positionsAttribute.array
 })
-const pointLight = new THREE.PointLight(0x00F00F, 3, 2500000, 0.001);
-scene.add(pointLight);
+// const pointLight = new THREE.PointLight(0x00F00F, 3, 2500000, 0.001);
+// scene.add(pointLight);
 
 
 
 function anima(particlePositions) {
-
-  for (let i = 0; i < quantPart * 3; i += 3) {
+  for (let i = 0; i < particlePositions.length; i += 3) {
     if (!palavra.checked) {
       particlePositions[i] += Math.random() * 2 - 1
       particlePositions[i + 1] += Math.random() * 2 - 1
@@ -271,15 +346,7 @@ function anima(particlePositions) {
       particlePositions[i] = newPosition.x
       particlePositions[i + 1] = newPosition.y
       particlePositions[i + 2] = newPosition.z
-
-      const time = performance.now() * 0.001;
-      const radius = 3000; 
-      const speed = 10; 
-      
-      const y = Math.cos(time * speed) * radius;
-      const z = Math.sin(time * speed) * radius;
-      pointLight.position.set(1000, y, z);
-      
+ 
     }
 
     if (aleat.checked) {
@@ -304,36 +371,83 @@ function anima(particlePositions) {
     }
   }
 }
-const clock = new THREE.Clock();
+// const clock = new THREE.Clock();
 const t = 0.01
 
-const iluminacao2 = new THREE.DirectionalLight(0x0f00ff, 1);
-scene.add(iluminacao2);
+// const iluminacao = new THREE.DirectionalLight(0x0f00ff, 1);
+// scene.add(iluminacao);
 
+// const assetLoader = new GLTFLoader();
 
-const assetLoader = new GLTFLoader();
-
-assetLoader.load(blend.href,function(gltf){
-  var model = gltf.scene;
-  scene.add(model);
+// assetLoader.load(blend.href,function(gltf){
+//   var model = gltf.scene;
+//   scene.add(model);
   
-model.position.set(0,0,0)
-//   mixer = new THREE.AnimationMixer(model);
-//   const clips = gltf.animations;
-//   const clip = THREE.AnimationClip.findByName(clips, 'HeadAction');
-// const action = mixer.clipAction(clip);
-// action.play();
+// model.position.set(-225,0,0)
+// //   mixer = new THREE.AnimationMixer(model);
+// //   const clips = gltf.animations;
+// //   const clip = THREE.AnimationClip.findByName(clips, 'HeadAction');
+// // const action = mixer.clipAction(clip);
+// // action.play();
 
-},undefined,function(error){
-  console.error(error);
+// },undefined,function(error){
+//   console.error(error);
 
-});
+// });
+function diminuiGaiola(particlePositions2) {
+  for (let i = 0; i < particlePositions2.length; i += 3) {
+    if (palavra.checked) {
+      const newPosition2 = {
+        x: particlePositions2[i],
+        y: particlePositions2[i + 1],
+        z: particlePositions2[i + 2],
+      }
+      const finalPosition = {
+        x: finalPositions2[i],
+        y: finalPositions2[i + 1],
+        z: finalPositions2[i + 2],
+      }
+      newPosition2.x += t * (finalPosition.x - newPosition2.x)
+      newPosition2.y += t * (finalPosition.y - newPosition2.y)
+      newPosition2.z += t * (finalPosition.z - newPosition2.z)
+
+      particlePositions2[i] = newPosition2.x
+      particlePositions2[i + 1] = newPosition2.y
+      particlePositions2[i + 2] = newPosition2.z
+ 
+    }
+
+    if (aleat.checked) {
+      const newPosition2 = {
+        x: particlePositions2[i],
+        y: particlePositions2[i + 1],
+        z: particlePositions2[i + 2],
+      }
+
+      const finalPosition2 = {
+        x: aleatPositions2[i],
+        y: aleatPositions2[i + 1],
+        z: aleatPositions2[i + 2],
+      }
+      newPosition2.x += t * (finalPosition2.x - newPosition2.x)
+      newPosition2.y += t * (finalPosition2.y - newPosition2.y)
+      newPosition2.z += t * (finalPosition2.z - newPosition2.z)
+
+      particlePositions2[i] = newPosition2.x
+      particlePositions2[i + 1] = newPosition2.y
+      particlePositions2[i + 2] = newPosition2.z
+    }
+}
+
+}
 
 function animate() {
-  if(mixer)
-  mixer.update(clock.getDelta());
+  // if(mixer)
+  // mixer.update(clock.getDelta());
   scene.background = new THREE.Color(corEsc.value)
+  particles2.material.color.set(corGaiola.value);
   let particlePositions = particles.geometry.attributes.position.array
+  let particlePositions2 = particles2.geometry.attributes.position.array
 
   if (clique === true) {
     if (textureMappings[selectedTexture] !== null) {
@@ -349,9 +463,20 @@ function animate() {
     clique = false
   }
   anima(particlePositions)
+  diminuiGaiola(particlePositions2)
 
+  // const time = performance.now() * 0.001;
+  // const radius = 3000; 
+  // const speed = 10; 
+  
+  // const y = Math.cos(time * speed) * radius;
+  // const z = Math.sin(time * speed) * radius;
+  // iluminacao.position.set(1000, y, z);
   particles.material.needsUpdate = true
   particles.geometry.attributes.position.needsUpdate = true
+
+  particles2.material.needsUpdate = true
+  particles2.geometry.attributes.position.needsUpdate = true
 
   camera.updateProjectionMatrix()
   renderer.render(scene, camera)
